a1d47bc721e9e5be4098e19b9faa66c99c0f3bd9
Matheus Gonzatti <matheus.gonzatti@outlook.com>
2019-09-22 16:25:20 -0300

    Feat: Add react hooks, redux and saga.

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
+import 'react-toastify/dist/ReactToastify.css';
 import background from '../assets/images/background.svg';
 
 export default createGlobalStyle`
@@ -13,7 +14,7 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #191920 url(${background}) no-repeat center top;
+    background: #fff url(${background}) no-repeat center top;
     -webkit-font-smoothing: antialiased !important;
     text-rendering: optimizeLegibility;
   }
